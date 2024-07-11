57d4d9ef1171719c3179f160dbaad42a2439dd90
DexterN00b <munna67.ms@gmail.com>
2024-07-11 21:42:02 +0500

    navbar without dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Lato', 'sans-serif'],
-      }
+        sans: ['Lato', 'sans-serif'],
+      },
+      colors: {
+        dark: '#131316',
+        paragraph: '#5e5f6e',
+        headerBtnDark: '#6e49ff',
+        headerBtnLight: '#9c80ff',
+        bodyHeadings: '#6c47ff',
+        ceoBg: '#615cf6',
+        bgNav: '#f4f5f6',
+        navFont: '#42434d',
+      },
     },
   },
   plugins: [],
-}
+};
 
